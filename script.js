bc11eb0946e8da3e23dae60803b9ba13099740c1
vivacenontroppo <vivacenontroppo@gmail.com>
2018-07-31 18:45:40 +0200

    quite working, quite looking, version 1

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const example = 1
+let inputElement = document.getElementById('in');
+const spanElement = document.getElementById('addBtn');
+
+const newTask = () => {
+    const ulElement = document.getElementById('ul');
+    const liElement = document.createElement('li');
+    liElement.appendChild(document.createTextNode(inputElement.value));
+    ulElement.appendChild(liElement);
+    inputElement.value = "";
+}
+
+inputElement.addEventListener('keydown', function(event){
+    if (event.keyCode == 13) {
+        newTask()
+    }
+});
+
+spanElement.addEventListener('click', newTask)
