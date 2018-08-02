78b9f157ce3a778319e42c16aa3cf9cdd2badb6e
Marian Kucharski <jannowak@example.com>
2018-08-02 16:09:39 +0200

    save checked to local and search than toggle them when geting data

--- script.js
+++ script.js
@@ -6,9 +6,17 @@ const closeBtn = document.getElementsByClassName('closeBtn');
 
 //prepare getting and setting data:
 const data = JSON.parse(localStorage.getItem('items'));
+const checkData = JSON.parse(localStorage.getItem('check'));
 let dataArray = data ? data : [];
-const setData = () => localStorage.setItem('items', JSON.stringify(dataArray));
-const getData = () => data ? data.forEach(item => {newTask(item)}) : [];
+let checkArray = checkData ? checkData : [];
+const setData = () => {
+    localStorage.setItem('items', JSON.stringify(dataArray));
+    localStorage.setItem('check', JSON.stringify(checkArray));
+};
+const getData = () => {
+    data ? data.forEach(item => {newTask(item)}) : [];
+    checkData ? checkData.forEach(check => setCheck(check)) : [];
+};
 
 //push input to localStorage, create new li element, clear input:
 const newTask = (text) => {
@@ -29,12 +37,25 @@ const newTask = (text) => {
     }
 };
 
+//search for checked before and check it again:
+const setCheck = (check) => {
+    const li = ulElement.getElementsByTagName("li");
+    for (let i = 0; i < li.length; i++) {
+        if (li[i].textContent == check) {
+        li[i].classList.toggle('checked'); 
+        }
+    }
+};
+
 getData();
 
 //listens for click on li and add line-through or remove:
 ulElement.addEventListener('click', (event) => {
     if (event.target.tagName === 'LI') {
-        event.target.classList.toggle('checked')
+        liText = event.target.textContent
+        event.target.classList.toggle('checked');
+        checkArray.push(liText);
+        setData();
     } else if (event.target.className === 'closeBtn') {
         parentText = event.target.parentElement.textContent.slice(0, -1);
         dataArray = dataArray.filter(e => e !== parentText);
