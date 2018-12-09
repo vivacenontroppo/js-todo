4ee658f46804804800340d2cc318cb97e7ae0d47
Marian Kucharski <marian.kucharski@itelo.pl>
2018-12-09 01:15:06 +0100

    trying to work with karma and jasmine for unit tests

---
+++ tests/unit/task-class.spec.js
@@ -0,0 +1,13 @@
+import { Task } from '../../src/task.class.js';
+
+describe('test task class', () => {
+
+task = new Task;
+
+    it('create an instance of task', () => {
+
+        expect('test').toBe('test')
+
+    });
+
+});
